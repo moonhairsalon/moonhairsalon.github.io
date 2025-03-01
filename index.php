27fcd0e729f0a0151ee9517afcc10234b8000261
moonhairsalon <thanhnd260801@gmail.com>
2025-03-01 14:10:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php include_once("index.html"); ?>
+<?php
+$request_uri = trim($_SERVER['REQUEST_URI'], '/');
+
+switch ($request_uri) {
+    case 'peace':
+        include_once("Peace.html");
+        break;
+    default:
+        include_once("index.html");
+        break;
+}
+?>
