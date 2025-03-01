cdd6e86f615e466cc81d5b423a71cb8e27c679ab
moonhairsalon <thanhnd260801@gmail.com>
2025-03-01 14:34:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,16 @@ $request_uri = trim($_SERVER['REQUEST_URI'], '/');
 
 switch ($request_uri) {
     case 'peace':
-        include_once("Peace.html");
+        include_once("peace.html");
+        break;
+    case 'sea':
+        include_once("sea.html");
+        break;
+    case 'namdinh':
+        include_once("namdinh.html");
+        break;
+    case 'denial':
+        include_once("denial.html");
         break;
     default:
         include_once("index.html");
